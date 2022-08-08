516fbd461d8d8792e84556865418784a5a0cf3f7
Wilson Icheku <ichekuwilson538@gmail.com>
2022-08-08 05:42:51 -0700

    hot fix

--- print.c
+++ print.c
@@ -14,8 +14,25 @@ for (i = 0; format[i] != '\0'; i++)
 			{
 			count = count + _putchar(format[i]);
 			}
-	        else if (format[i] == '%' && format[i+1] != ' ')
-		switch (
+	        else if (format[i] == '%' && format[i + 1] != ' ')
+			{
+			switch(format[i + 1])
+				{
+				case 'c':
+					count += _putchar(args(list,int));
+					break;
+				case 's':
+					count += _putchar(args(list,char*);
+                                        break;
+				case '%':
+				        count += _putchar('%');
+					break;
+				default :
+				       
+				        
+				}
+			}
+	}
 
 return count;
 }
